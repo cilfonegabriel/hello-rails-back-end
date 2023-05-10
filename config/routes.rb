e4ea7c8c399ed491b2965f833414e01a7ff1664a
gabicilfone <cilfonegabriel@gmail.com>
2023-05-10 14:44:29 -0300

    add wievs, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'api/greetings#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    resources :greetings, only: [:index]
+    get '/greetings/random', to: 'greetings#random'
+  end
 end
