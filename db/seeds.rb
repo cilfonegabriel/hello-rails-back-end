b325c4f9d54a1d753c3ea868e2f74b5407e84c81
gabicilfone <cilfonegabriel@gmail.com>
2023-05-11 09:07:24 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello! How are you today?")
+Greeting.create(message: "Hi there! I hope you're having a great day.")
+Greeting.create(message: "Good morning! I hope you slept well.")
+Greeting.create(message: "Hey, how's it going? What's new?")
+Greeting.create(message: "Greetings and salutations! I trust you're doing well.")
